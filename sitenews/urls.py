c92aeaa78637a119808268241b8dd82c16d6b5a6
Andrii Dobrovolskyi <andrii.dobrovolskui@sigmaukraine.com>
2016-05-21 15:49:34 +0300

    renames

--- sitenews/urls.py
+++ sitenews/urls.py
@@ -1,4 +1,4 @@
-"""shopbox URL Configuration
+"""sitenews URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
